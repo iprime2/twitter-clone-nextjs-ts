da252b0d8e432cc4095e5493ac980abc849aa1d1
Sushil Gupta <kalwarsushil2@gmail.com>
2023-05-29 15:18:16 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const config = {
-  api: {
-    responseLimit: false,
-    // responseLimit: '8mb',
-  },
-}
+module.exports = nextConfig
 
-module.exports = { nextConfig, config }
+
 
